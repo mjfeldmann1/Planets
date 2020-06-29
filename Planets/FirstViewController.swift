252ebdde5aaaffb23f113290d0a92336baacd5e2
Michael Feldmann <mjfeldmann1@gmail.com>
2020-06-29 09:52:44 -0500

    first commit

---
+++ Planets/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Planets
+//
+//  Created by Michael Feldmann on 6/29/20.
+//  Copyright © 2020 Bounding-22 LLC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
